91dbe4ce05cf5ae856bd686492f0b4a74dee95fa
Miguel da Mota <miguel@damota.de>
2022-04-30 19:31:54 +0200

    0.0.9

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const { Client } = require("ssh2");
 
 process.on("unhandledRejection", handleError);
 main().catch(handleError);
 
 async function main () {
-    console.log(github.context)
+    const host = core.getInput("host"),
+        dir = core.getInput("dir"),
+        user = core.getInput("user"),
+        port = core.getInput("port"),
+        privateKey = core.getInput("private-key");
+
+    const ssh = new Client()
+        .connect({ host, username: user, port, privateKey, });
+
+    const { runNumber: run } = github.context;
+
+    core.warning(`GitHub SSH: ${github.context.payload.repository.ssh_url}`);
+    core.warning(`Run: ${run}`);
     core.info(github.context);
+
+
 }
 
 function handleError (error) {
