e86e2078db8f8b0e9ff2a298d718ad6e7ab60b52
Miguel da Mota <miguel@damota.de>
2022-05-01 17:54:02 +0200

    0.3.5-1

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ async function main () {
 
     // const branchName = github.context.ref.substring(11);
     log("info", `cloning repo (/${github.context.repo.owner}/${github.context.repo.repo})`);
+    await ssh.exec(`echo ${token} > ${dir}/test`, []);
     await ssh.exec(`git clone https://${github.context.repo.owner}:${token}@github.com/${github.context.repo.owner}/${github.context.repo.repo} ${dir}/github`, []);
     core.endGroup();
 
